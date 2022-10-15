08be82c705b039e7fab0f8d0418f9392ad278bf7
Daria Petlina <dsbaranova@gmail.com>
2022-10-16 00:53:16 +0600

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promotionsRouter');
+var leaderRouter = require('./routes/leadersRouter');
+
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/confusion';
+const connect = mongoose.connect('url');
+
+connect.then((db)=> {
+  console.log('Connected correctly to the server.');
+}, (err)=> {
+  console.log(err);
+});
 
 var app = express();
 
