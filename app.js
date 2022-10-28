76c5fc75ec6d53fffeb51739224a5cb0fe26fada
Daria Petlina <dsbaranova@gmail.com>
2022-10-29 01:35:49 +0600

    Basic Authentication

--- app.js
+++ app.js
@@ -35,6 +35,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  let authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    let err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  let auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  let username = auth[0];
+  let password = auth[1];
+
+  if (username === 'admin' && password === 'password') {
+    return next();
+  } else {
+    let err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
