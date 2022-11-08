9dcb1f058ec3fb069ffcadd19693a63836fe56b6
Daria Petlina <dsbaranova@gmail.com>
2022-11-08 23:54:46 +0600

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -5,6 +5,14 @@ const Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new Schema({
+	firstName:   {
+		type: String,
+		default: ''
+	},
+	lastName:   {
+		type: String,
+		default: ''
+	},
 	admin:   {
 		type: Boolean,
 		default: false
--- routes/users.js
+++ routes/users.js
@@ -15,21 +15,36 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}),
-      req.body.password, (err, user) => {
-        if(err) {
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({err: err});
-        }
-        else {
-          passport.authenticate('local')(req, res, () => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({success: true, status: 'Registration Successful!'});
-          });
-        }
-      });
+	User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+		if (err) {
+			res.statusCode = 500;
+			res.setHeader('Content-Type', 'application/json');
+			res.json({err: err});
+		} else {
+			if (req.body.firstName) {
+				user.firstName = req.body.firstName;
+			}
+			if (req.body.lastName) {
+				user.lastName = req.body.lastName;
+			}
+			user.save((err, user) => {
+				if (err) {
+					res.statusCode = 500;
+					res.setHeader('Content-Type', 'application/json');
+					res.json({err: err});
+					return;
+				} else {
+					passport.authenticate('local')(req, res, () => {
+						res.statusCode = 200;
+						res.setHeader('Content-Type', 'application/json');
+						res.json({success: true, status: 'Registration Successful!'});
+					});
+				}
+
+			});
+
+		}
+	});
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
