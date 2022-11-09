17b6f4c2554aa0ee5347efcaf2703e7eabc96836
Daria Petlina <dsbaranova@gmail.com>
2022-11-09 17:31:13 +0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	'secretKey': '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://127.0.0.1:27017/confusion'
+	'mongoUrl': 'mongodb://127.0.0.1:27017/confusion',
+	'facebook': {
+		clientId: '668797954704463',
+		clientSecret: 'a2b727a587df7cafb87cc923f5da89d9'
+	}
 }
--- models/user.js
+++ models/user.js
@@ -13,6 +13,7 @@ var UserSchema = new Schema({
 		type: String,
 		default: ''
 	},
+	facebookId: String,
 	admin:   {
 		type: Boolean,
 		default: false
